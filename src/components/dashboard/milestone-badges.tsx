c067bcb43e70840c1d3db88531ca90d0c3cec236
Jonathan Vilario <JonVilario@gmail.com>
2025-07-12 21:26:24 +0000

    I don't see all the badges in the milestone badges dashboard

--- src/components/dashboard/milestone-badges.tsx
+++ src/components/dashboard/milestone-badges.tsx
@@ -10,7 +10,7 @@ import { Button } from '../ui/button';
 import Link from 'next/link';
 
 export function MilestoneBadges() {
-  const displayedBadges = badges.slice(0, 8);
+  const displayedBadges = badges;
 
   return (
     <Card className="hover:shadow-lg transition-shadow duration-300 flex flex-col">
