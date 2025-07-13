feec7094d219b3a349ef261b9f26f47c7d4dd4dc
Jonathan Vilario <JonVilario@gmail.com>
2025-07-13 20:09:57 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={`${inter.variable} font-sans antialiased`}>
+      <body className={`${inter.variable} font-sans antialiased`} suppressHydrationWarning={true}>
         <ThemeProvider>
           <TrainingFocusProvider>
             {children}
