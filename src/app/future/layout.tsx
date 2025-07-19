578e03ee9ee5f5a93339b3eb3e10c594c12363c6
Jonathan Vilario <JonVilario@gmail.com>
2025-07-19 00:01:36 +0000

    I can't click the future page

---
+++ src/app/future/layout.tsx
@@ -0,0 +1,25 @@
+
+import { Header } from '@/components/header';
+import { PageNav } from '@/components/page-nav';
+import { MotivationalMessage } from '@/components/motivational-message';
+
+export default function FutureLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <>
+      <div className="sticky top-0 z-20">
+        <Header />
+        <PageNav />
+      </div>
+      <MotivationalMessage />
+      <main className="flex-1 p-4 sm:p-6 md:p-8">
+        <div className="mx-auto max-w-7xl">
+            {children}
+        </div>
+      </main>
+    </>
+  );
+}
