6d0bc872ea6231ed350bb6957eb3e657f88914bb
Jonathan Vilario <JonVilario@gmail.com>
2026-02-14 05:40:58 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -16,6 +16,17 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+  experimental: {
+    allowedDevOrigins: [
+      '6000-firebase-polymath-lab-1754437488437.cluster-f4iwdviaqvc2ct6pgytzw4xqy4.cloudworkstations.dev',
     ],
   },
 };
