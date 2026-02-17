2fb847b6eea4935bd35dd932db498eb0f71aff06
Jonathan Vilario <JonVilario@gmail.com>
2026-02-17 17:02:36 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "polymath-lab-44749302-a8016",
+  "appId": "1:342990639715:web:515bb36cc3189003831e56",
+  "apiKey": "AIzaSyCGji_jkJldPzcJdRr0tGH3Tnjp0YYq0fg",
+  "authDomain": "polymath-lab-44749302-a8016.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "342990639715"
+};
